e9d16359372dece903619f0f0871a6a9eb4c9fee
Frans Manders <frans.manders@gmail.com>
2018-03-28 21:49:55 +0200

    Leaking memory fix

--- src/SimpleEvaluator.cpp
+++ src/SimpleEvaluator.cpp
@@ -193,6 +193,7 @@ std::vector<std::string> SimpleEvaluator::getPrioritizedAST_aux(std::vector<std:
     std::string query = queryParts[i-1] + "/" + queryParts[i];
     auto queryTree = RPQTree::strToTree(query);
     cardinalities[i-1] = est->estimate(queryTree).noPaths;
+    delete queryTree;
   }
 
   int min_cardinality = 9999999;
